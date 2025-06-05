fae4864df2c19115bc5f378cbcdf74b2154e6fde
gabrielcarsa <gabrielcarsa@hotmail.com>
2025-06-05 10:59:06 -0400

    feat (Enterprise): create and list

--- debits/urls.py
+++ debits/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from debits.views import EnterpriseListView
+from debits.views import EnterpriseCreateView, EnterpriseListView
 
 
 urlpatterns = [
     path('enterprise/list', EnterpriseListView.as_view(), name='enterprise_list'),
+    path('enterprise/create', EnterpriseCreateView.as_view(), name='enterprise_create'),
 
 ]
--- debits/views.py
+++ debits/views.py
@@ -1,4 +1,6 @@
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from debits.models import Enterprise
@@ -8,3 +10,16 @@ class EnterpriseListView(LoginRequiredMixin, ListView):
     model = Enterprise
     template_name = 'enterprise/list.html'
     paginate_by = 100
+
+class EnterpriseCreateView(LoginRequiredMixin, CreateView):
+    model = Enterprise
+    fields = ['name', 'city', 'state', 'property_registration']
+    template_name = 'enterprise/form.html'
+    success_url = reverse_lazy('enterprise_list')
+
+    def form_valid(self, form):
+
+        form.instance.created_by_user = self.request.user
+        form.instance.updated_by_user = self.request.user
+
+        return super().form_valid(form)
