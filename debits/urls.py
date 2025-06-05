e4406d461c105c42e4ed8d1a3fd5f562757be4f5
gabrielcarsa <gabrielcarsa@hotmail.com>
2025-06-05 16:53:46 -0400

    feat(Enterprise): delete

--- debits/urls.py
+++ debits/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from debits.views import EnterpriseCreateView, EnterpriseListView, EnterpriseUpdateView
+from debits.views import EnterpriseCreateView, EnterpriseDeleteView, EnterpriseListView, EnterpriseUpdateView
 
 
 urlpatterns = [
     path('enterprise/list', EnterpriseListView.as_view(), name='enterprise_list'),
     path('enterprise/create', EnterpriseCreateView.as_view(), name='enterprise_create'),
     path('enterprise/update/<int:pk>', EnterpriseUpdateView.as_view(), name='enterprise_update'),
+    path('enterprise/delete/<int:pk>', EnterpriseDeleteView.as_view(), name='enterprise_delete'),
+
 
 ]
--- debits/views.py
+++ debits/views.py
@@ -1,8 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib import messages
 from debits.models import Enterprise
 
 
@@ -21,7 +21,7 @@ class EnterpriseCreateView(LoginRequiredMixin, CreateView):
 
         form.instance.created_by_user = self.request.user
         form.instance.updated_by_user = self.request.user
-
+        messages.success(self.request, 'Operação realizada com sucesso')
         return super().form_valid(form)
     
 class EnterpriseUpdateView(LoginRequiredMixin, UpdateView):
@@ -33,5 +33,15 @@ class EnterpriseUpdateView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
 
         form.instance.updated_by_user = self.request.user
+        messages.success(self.request, 'Operação realizada com sucesso')
+        return super().form_valid(form)
+    
+class EnterpriseDeleteView(LoginRequiredMixin, DeleteView):
+    model = Enterprise
+    template_name = 'enterprise/delete.html'
+    success_url = reverse_lazy('enterprise_list')
 
+    def form_valid(self, form):
+
+        messages.success(self.request, 'Operação realizada com sucesso')
         return super().form_valid(form)
