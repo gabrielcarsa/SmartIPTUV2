b8d50cbe0541ce6739b4e7847f4d0510502e18d0
gabrielcarsa <gabrielcarsa@hotmail.com>
2025-06-05 11:04:09 -0400

    feat (Enterprise): update

--- debits/urls.py
+++ debits/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from debits.views import EnterpriseCreateView, EnterpriseListView
+from debits.views import EnterpriseCreateView, EnterpriseListView, EnterpriseUpdateView
 
 
 urlpatterns = [
     path('enterprise/list', EnterpriseListView.as_view(), name='enterprise_list'),
     path('enterprise/create', EnterpriseCreateView.as_view(), name='enterprise_create'),
+    path('enterprise/update/<int:pk>', EnterpriseUpdateView.as_view(), name='enterprise_update'),
 
 ]
--- debits/views.py
+++ debits/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from debits.models import Enterprise
@@ -20,6 +20,18 @@ class EnterpriseCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
 
         form.instance.created_by_user = self.request.user
+        form.instance.updated_by_user = self.request.user
+
+        return super().form_valid(form)
+    
+class EnterpriseUpdateView(LoginRequiredMixin, UpdateView):
+    model = Enterprise
+    fields = ['name', 'city', 'state', 'property_registration']
+    template_name = 'enterprise/form.html'
+    success_url = reverse_lazy('enterprise_list')
+
+    def form_valid(self, form):
+
         form.instance.updated_by_user = self.request.user
 
         return super().form_valid(form)
