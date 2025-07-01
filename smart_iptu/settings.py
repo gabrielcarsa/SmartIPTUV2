c176d1e5dcb23664de37ed0faeaad9536574d532
gabrielcarsa <gabrielcarsa@hotmail.com>
2025-07-01 09:25:42 -0400

    refactor: hide SECRET_KEY

--- smart_iptu/settings.py
+++ smart_iptu/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_-51_*0$)pp2rlot6f6a$^ej1=n1ud=tgq_26v2qm#cp06!baz'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
