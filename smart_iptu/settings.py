d6c2d27bdba718d7abd9d34176f83506623e4394
gabrielcarsa <gabrielcarsa@hotmail.com>
2025-07-01 14:04:28 -0400

    fix: error production HOST

--- smart_iptu/settings.py
+++ smart_iptu/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+load_dotenv()  # Carrega as variáveis do .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,6 @@ WSGI_APPLICATION = 'smart_iptu.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-load_dotenv()  # Carrega as variáveis do .env
 
 DATABASES = {
     "default": {
