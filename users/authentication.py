2ba26e00c1f6eb6884fc8d60e8a6e7a8de66f7a9
gabrielcarsa <gabrielcarsa@hotmail.com>
2025-07-01 10:05:45 -0400

    fix: user authentication with email on production

--- users/authentication.py
+++ users/authentication.py
@@ -1,8 +1,11 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
 class EmailBackend(ModelBackend):
+
     def authenticate(self, request, username=None, password=None, **kwargs):
+
+        User = get_user_model()
 
         # Try autenticate 
         if username is None:
