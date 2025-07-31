bc466fb51972ea002210c9ba037be0b6aef62cb1
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2025-07-31 18:01:00 -0300

    refactor: ordenando por nome CustomerSupplier

--- financials/forms.py
+++ financials/forms.py
@@ -191,7 +191,7 @@ class OFXMovementForm(BaseForm):
     )
 
     customer_supplier = forms.ModelChoiceField(
-        queryset=CustomerSupplier.objects.all(),
+        queryset=CustomerSupplier.objects.all().filter('name'),
         required=True,
         widget=forms.Select()
     )
