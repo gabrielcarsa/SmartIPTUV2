96c76029fbf174981ce4ad9d1aa7df578a083a21
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2025-08-13 15:30:45 -0400

    fix: formset

--- financials/views.py
+++ financials/views.py
@@ -608,7 +608,7 @@ class MovementImportSaveView(LoginRequiredMixin, View):
     def post(self, request):
         
         # formset
-        formset = MovimentsFormSet(request.POST)
+        formset = MovimentsFormSet(request.POST or None, queryset=models.FinancialMovement.objects.none())
 
         if formset.is_valid():
             
